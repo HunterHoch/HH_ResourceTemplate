4c1f8e54903a65ee22fdec129bcc44f990bc9ff7
HunterHoch <hunterhoch2@gmail.com>
2024-07-13 19:02:26 -0700

    Template V1

---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Hunter Hoch'
+description 'A personalized resource template for FiveM'
+version '1.0.0'
+
+client_scripts {
+    'scripts/client/*.lua'
+}
+
+server_scripts {
+    'scripts/server/*.lua'
+}
+
+-- # Delete if not using # --
+-- files {
+--     'data/**/**/*.meta',
+--     'data/**/**/*.xml',
+--     'data/**/**/*.dat',
+--     'data/**/**/*.ytyp'
+-- }
+
+-- # Delete if not using # --
+-- data_file 'HANDLING_FILE'                   'data/**/**/handling.meta'
+-- data_file 'VEHICLE_LAYOUT_FILE'             'data/**/**/vehiclelayouts.meta'
+-- data_file 'VEHICLE_METADATA_FILE'           'data/**/**/vehicles.meta'
+-- data_file 'CARCOLS_FILE'                    'data/**/**/carcols.meta'
+-- data_file 'VEHICLE_VARIATION_FILE'          'data/**/**/carvariatons.meta'
+-- data_file 'CONTENT_UNLOCKING_META_FILE'     'data/**/**/*unlocks.meta'
+-- data_file 'PTFXASSETINFO_FILE'              'data/**/**/ptfxassetinfo.meta'
